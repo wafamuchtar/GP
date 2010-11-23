b2413b7ce28d320372e068edd718260381874707
wafamuchtar <wafamuchtar@gmail.com>
2010-11-23 08:26:55 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GP::Application.config.secret_token = 'e94277abc9d91837e65e458c45e5eab9db2fdc061bc16fa62346f60e154f21a40e97bf7b619e6c26c49f9b5ff991269c45483bce1640befbd4f1d3a672699356'
